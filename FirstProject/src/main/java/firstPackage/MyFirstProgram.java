5e578fdca0f67c1589b5d71ba06bff9e79ca20ad
sanjar jarash <sjarashev@gmail.com>
2019-11-17 00:26:59 -0500

    изменены насройки отступов + проект отвязан от Git

--- FirstProject/src/main/java/firstPackage/MyFirstProgram.java
+++ FirstProject/src/main/java/firstPackage/MyFirstProgram.java
@@ -2,10 +2,10 @@ package firstPackage;
 
 public class MyFirstProgram {
 
-public static void main(String[] args) {
+  public static void main(String[] args) {
 
-System.out.println("Hello World!");
+    System.out.println("Hello World!");
 
-}
+  }
 
 }
