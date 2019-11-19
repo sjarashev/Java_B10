7cdb8f8e07f74d3e006b3fbc43f54d14f11b7e98
sanjar jarash <sjarashev@gmail.com>
2019-11-19 12:12:11 -0500

    Исправлена домашняя работа к Заданию #2

--- FirstProject/src/main/java/firstPackage/MyFirstProgram.java
+++ FirstProject/src/main/java/firstPackage/MyFirstProgram.java
@@ -8,12 +8,8 @@ public class MyFirstProgram {
 
     Point p2 = new Point(4, 5);
 
-    System.out.println("Растояние между точками p1 и p2 = " + distance(p1,p2));
+    System.out.println("Растояние между точками p1 и p2 = " + p1.distance(p2));
 
-  }
-
-  public static double distance(Point p1, Point p2) {
-    return (Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2)));
   }
 
 }
