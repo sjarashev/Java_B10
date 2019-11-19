9417340398435ad539209bf3a445905682f82139
sanjar jarash <sjarashev@gmail.com>
2019-11-19 01:01:57 -0500

    Исправленная домашняя работа к Заданию №2
    Я сделал все как указано в задании за исключением последнего пункта, в котором говорится о перенесении функции "public static double distance(Point p1, Point p2)" из запускаемого класса в класс "Point", таким образом переделав ее в метод. Когда я это делаю итог вычисления всегда получается 0.0, потому что ключевое слово "this" не знает на какой из объектов класса ссылаться. Вот пример: "return (Math.sqrt(Math.pow(this.x - this.x, 2) + Math.pow(this.y - this.y, 2)));" Вы примете мою работу в данном виде?

--- FirstProject/src/main/java/firstPackage/MyFirstProgram.java
+++ FirstProject/src/main/java/firstPackage/MyFirstProgram.java
@@ -4,10 +4,16 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
 
-    Run r = new Run();
-    r.resultOfCalculation();
+    Point p1 = new Point(2, 3);
 
-    /* r.humanWeight(); */
+    Point p2 = new Point(4, 5);
+
+    System.out.println("Растояние между точками p1 и p2 = " + distance(p1,p2));
+
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return (Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2)));
   }
 
 }
--- FirstProject/src/main/java/firstPackage/Point.java
+++ FirstProject/src/main/java/firstPackage/Point.java
@@ -2,17 +2,13 @@ package firstPackage;
 
 public class Point {
 
-  public double p1;
-  public double p2;
+  public double x;
+  public double y;
 
-  public Point(double p1, double p2){
-    this.p1 = p1;
-    this.p2 = p2;
-  }
 
-  public double calculateDistance(){
-    double r = this.p2 - this.p1;
-    return r;
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
   }
 
 }
