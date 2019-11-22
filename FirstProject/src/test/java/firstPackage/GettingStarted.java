189f45b15fc1db842b8f074c3586056cf766271f
sanjar jarash <sjarashev@gmail.com>
2019-11-21 22:48:40 -0500

    Задание 3. Сделайте сценарий логина

--- FirstProject/src/test/java/firstPackage/GettingStarted.java
+++ FirstProject/src/test/java/firstPackage/GettingStarted.java
@@ -1,21 +1,44 @@
 package firstPackage;
 
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.*;
-import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
 
 public class GettingStarted {
+
   @Test
-  public void openGooglePage() throws InterruptedException {
+  public void loginToLitecard() throws Exception {
 
-    System.setProperty("webdriver.chrome.driver", "C:\\Users\\sjarashev\\Desktop\\Java Training\\Java_B10\\FirstProject\\Webdriver\\chromedriver_V78.exe");
-
+    System.setProperty("webdriver.chrome.driver", "C:\\Users\\sjarashev\\Desktop\\Java Training\\Java_B10\\FirstProject\\webdriver\\chromedriver_T78.exe");
     WebDriver driver = new ChromeDriver();
-    driver.get("http://www.google.com/");
-    Thread.sleep(1000);
-    driver.manage().window().maximize();
-    Thread.sleep(2000);
-    driver.quit();
+
+    try {
+      driver.get("http://localhost/litecart/admin");
+      Thread.sleep(1000);
+      driver.manage().window().maximize();
+      Thread.sleep(1000);
+      driver.findElement(By.name("username")).sendKeys("admin");
+      Thread.sleep(1000);
+      driver.findElement(By.name("password")).sendKeys("admin");
+      Thread.sleep(1000);
+      driver.findElement(By.name("login")).click();
+      Thread.sleep(1000);
+      driver.findElement(By.xpath("//span[contains(text(), 'Appearence')]")).click();
+      Thread.sleep(1000);
+      driver.findElement(By.xpath("//span[contains(text(), 'Template')]")).click();
+      Thread.sleep(1000);
+      driver.findElement(By.xpath("//i[contains(@class, 'fa fa-sign-out fa-lg')]")).click();
+      Thread.sleep(2000);
+      driver.quit();
+
+    } catch (Exception e) {
+      System.out.println(e);
+      driver.quit();
+    }
   }
 }
+
+
 
