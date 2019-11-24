4e2bf6f5f331aa2d1b9f2185f53327a3203ea0ca
sanjar jarash <sjarashev@gmail.com>
2019-11-23 22:59:46 -0500

    Промежуточный комит после лекции 2.2

--- FirstProject/src/test/java/firstPackage/GettingStarted.java
+++ FirstProject/src/test/java/firstPackage/GettingStarted.java
@@ -3,16 +3,45 @@ package firstPackage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.*;
+
+import java.io.File;
 
 
 public class GettingStarted {
 
-  @Test
-  public void loginToLitecard() throws Exception {
 
-    System.setProperty("webdriver.chrome.driver", "C:\\Users\\sjarashev\\Desktop\\Java Training\\Java_B10\\FirstProject\\webdriver\\chromedriver_T78.exe");
-    WebDriver driver = new ChromeDriver();
+
+  @Test
+  public void loginToLitecard() {
+
+    System.setProperty("webdriver.gecko.driver", "C:\\Users\\sjarashev\\Desktop\\Java Training\\Java_B10\\FirstProject\\webdriver\\geckodriver0.24_win64.exe");
+    WebDriver driver = new FirefoxDriver();
+
+    // старая схема более явно (этот способ указания опций рекомендуется, начиная с версии 3.3):
+    //FirefoxOptions options = new FirefoxOptions().setLegacy(true);
+    //WebDriver driver = new FirefoxDriver(options);
+
+    //старая схема (при использовании Selenium до версии 3.3):
+    //DesiredCapabilities caps = new DesiredCapabilities();
+    //caps.setCapability(FirefoxDriver.MARIONETTE, true);
+    //WebDriver driver = new FirefoxDriver(caps);
+
+     //при использовании Selenium 3.3 и новее, с девелоперской версией или FirefoxNightly:
+    //System.setProperty("webdriver.gecko.driver", "C:\\Users\\sjarashev\\Desktop\\Java Training\\Java_B10\\FirstProject\\webdriver\\geckodriver0.24_win64.exe");
+    //FirefoxOptions options = new FirefoxOptions();
+    //options.setBinary(new FirefoxBinary(new File("C:\\Program Files\\Firefox Nightly\\firefox.exe")));
+    //WebDriver driver = new FirefoxDriver(options);
+
+    // при использовании версий до 3.3 , с девелоперской версией или FirefoxNightly:
+    //FirefoxBinary bin = new FirefoxBinary(new File("c:\\Program Files\\Firefox Nightly\\firefox.exe"));
+    //WebDriver driver = new FirefoxDriver(bin, new FirefoxProfile());
 
     try {
       driver.get("http://localhost/litecart/admin");
