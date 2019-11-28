026e17df566a834973cd584e1e95bdae530c25d4
sanjar jarash <sjarashev@gmail.com>
2019-11-27 23:16:41 -0500

    Исправленная домашняя работа к Заданию №4

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,18 +1,106 @@
 package ru.stqa.pft.addressbook.model;
 
 public class ContactData {
-  private final String companyURL;
-  private final String email;
-  private final String workPhone;
-  private final String mobilePhone;
-  private final String homePhone;
 
-  public ContactData(String companyURL, String email, String workPhone, String mobilePhone, String homePhone) {
-    this.companyURL = companyURL;
-    this.email = email;
+  private String name;
+  private String lastName;
+  private String nickName;
+  private String title;
+  private String companyName;
+  private String companyAddress;
+  private String companyURL;
+  private String email;
+  private String workPhone;
+  private String mobilePhone;
+  private String homePhone;
+  private String year;
+  private String month;
+  private String day;
+  private String note;
+  private String secondPhone;
+  private String secondAddress;
+
+  public ContactData(String name, String lastName, String nickName, String title) {
+    this.name = name;
+    this.lastName = lastName;
+    this.nickName = nickName;
+    this.title = title;
+  }
+
+  public void setCompanyName(String companyName) {
+    this.companyName = companyName;
+  }
+
+  public void setCompanyAddress(String companyAddress) {
+    this.companyAddress = companyAddress;
+  }
+
+  public void setWorkPhone(String workPhone) {
     this.workPhone = workPhone;
+  }
+
+  public void setMobilePhone(String mobilePhone) {
     this.mobilePhone = mobilePhone;
+  }
+
+  public void setHomePhone(String homePhone) {
     this.homePhone = homePhone;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setCompanyURL(String companyURL) {
+    this.companyURL = companyURL;
+  }
+
+  public void setYear(String year) {
+    this.year = year;
+  }
+
+  public void setMonth(String month) {
+    this.month = month;
+  }
+
+  public void setDay(String day) {
+    this.day = day;
+  }
+
+  public void setNote(String note) {
+    this.note = note;
+  }
+
+  public void setSecondPhone(String secondPhone) {
+    this.secondPhone = secondPhone;
+  }
+
+  public void setSecondAddress(String secondAddress) {
+    this.secondAddress = secondAddress;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getNickName() {
+    return nickName;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getCompanyName() {
+    return companyName;
+  }
+
+  public String getCompanyAddress() {
+    return companyAddress;
   }
 
   public String getCompanyURL() {
@@ -34,4 +122,32 @@ public class ContactData {
   public String getHomePhone() {
     return homePhone;
   }
+
+  public String getYear() {
+    return year;
+  }
+
+  public String getMonth() {
+    return month;
+  }
+
+  public String getDay() {
+    return day;
+  }
+
+  public String getNote() {
+    return note;
+  }
+
+  public String getSecondPhone() {
+    return secondPhone;
+  }
+
+  public String getSecondAddress() {
+    return secondAddress;
+  }
 }
+
+
+
+
