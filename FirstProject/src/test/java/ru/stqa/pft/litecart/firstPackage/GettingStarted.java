46f3b962f250f0f16f591efa6c3ed207fbfa65d7
sanjar jarash <sjarashev@gmail.com>
2019-11-30 22:36:54 -0500

    Домашняя работа к заданию №8

--- FirstProject/src/test/java/ru/stqa/pft/litecart/firstPackage/GettingStarted.java
+++ FirstProject/src/test/java/ru/stqa/pft/litecart/firstPackage/GettingStarted.java
@@ -5,14 +5,16 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.*;
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class GettingStarted {
 
-  @Test
-  public void loopThroughMenuItems() throws InterruptedException {
+/*  @Test
+   public void loopThroughMenuItems() throws InterruptedException {
     System.setProperty("webdriver.gecko.driver", "C:\\Users\\sjarashev\\Desktop\\Java Training\\Java_B10\\FirstProject\\webdriver\\geckodriver0.24_win64.exe");
     WebDriver driver = new FirefoxDriver();
     driver.manage().timeouts().implicitlyWait(200, TimeUnit.MILLISECONDS);
@@ -41,6 +43,36 @@ public class GettingStarted {
           System.out.println(present);
         }
       }
+    }
+    driver.quit();
+  }*/
+
+
+  @Test
+  public void checkStickers() throws InterruptedException {
+    System.setProperty("webdriver.gecko.driver", "C:\\Users\\sjarashev\\Desktop\\Java Training\\Java_B10\\FirstProject\\webdriver\\geckodriver0.24_win64.exe");
+    WebDriver driver = new FirefoxDriver();
+    driver.manage().timeouts().implicitlyWait(200, TimeUnit.MILLISECONDS);
+    driver.get("http://localhost/litecart/en/");
+    driver.manage().window().maximize();
+
+    List<WebElement> duck = driver.findElements(By.xpath("//li[contains(@class, 'product')]"));
+    List<WebElement> sticker = driver.findElements(By.xpath("//div[contains(@class, 'sticker')]"));
+
+    int d = duck.size();
+    int s = sticker.size();
+    Assert.assertEquals(d, s);
+
+    for (int i = 1; i <= duck.size(); i++) {
+      driver.findElements(By.xpath("//li[contains(@class, 'product')][" + i + "]"));
+      boolean present;
+      try {
+        driver.findElement(By.xpath("//div[contains(@class, 'sticker')]"));
+        present = true;
+      } catch (NoSuchElementException e) {
+        present = false;
+      }
+      Assert.assertTrue(present);
     }
     driver.quit();
   }
