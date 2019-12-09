954091fb43ad44ac2702084e7f646bdf350a550d
sanjar jarash <sjarashev@gmail.com>
2019-12-09 09:19:34 -0500

    Исправлена домашняя работа к заданию №7

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import sun.plugin2.util.BrowserType;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,18 +16,18 @@ public class ApplicationManager {
   private ContactHelper contactHelper;
   private GroupHelper groupHelper;
   private SessionHelper sessionHelper;
-  private int browser;
+  private String browser;
 
-  public ApplicationManager(int browser) {
+  public ApplicationManager(String browser) {
     this.browser = browser;
   }
 
   public void init() {
-    if (browser == BrowserType.MOZILLA) {
+    if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
-    } else if (browser == BrowserType.DEFAULT) {
+    } else if (browser.equals(BrowserType.CHROME)) {
       wd = new ChromeDriver();
-    } else if (browser == BrowserType.INTERNET_EXPLORER) {
+    } else if (browser.equals(BrowserType.IE)) {
       wd = new InternetExplorerDriver();
     }
     //wd.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -3,12 +3,12 @@ package ru.stqa.pft.addressbook.tests;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import sun.plugin2.util.BrowserType;
+import org.openqa.selenium.remote.BrowserType;
 
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.MOZILLA);
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() {
