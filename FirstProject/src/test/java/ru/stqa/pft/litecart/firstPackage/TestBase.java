8ba22e751323f94d8bf93a417041f3efc71b1518
sanjar jarash <sjarashev@gmail.com>
2019-12-19 04:11:33 -0500

    Промежуточный комит

--- FirstProject/src/test/java/ru/stqa/pft/litecart/firstPackage/TestBase.java
+++ FirstProject/src/test/java/ru/stqa/pft/litecart/firstPackage/TestBase.java
@@ -10,12 +10,12 @@ import java.util.concurrent.TimeUnit;
 
 public class TestBase {
 
-  EdgeDriver wd;
+  FirefoxDriver wd;
 
   @BeforeMethod
   public void init() {
     //System.setProperty("webdriver.gecko.driver", "C:\\Users\\sjarashev\\Desktop\\Java Training\\Java_B10\\FirstProject\\webdriver\\geckodriver0.24_win64.exe");
-    wd = new EdgeDriver();
+    wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
     //wd.get("http://localhost/litecart/admin/?app=countries&doc=countries");
     //wd.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones");
