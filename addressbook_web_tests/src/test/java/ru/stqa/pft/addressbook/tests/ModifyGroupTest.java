9da0306234e997d76b49dfd59885988425388bbc
sanjar jarash <sjarashev@gmail.com>
2019-12-28 19:38:41 -0500

    a

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/ModifyGroupTest.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/tests/ModifyGroupTest.java
@@ -6,35 +6,33 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 
 public class ModifyGroupTest extends TestBase {
 
   @BeforeMethod
   public void ensurePreconditions(){
     app.goTo().groupPage();
-    if (app.group().list().size()==0){
+    if (app.group().all().size()==0){
       app.group().create(new GroupData().withName("group").withHeader("header").withFooter("footer"));
     }
   }
 
-  @Test(enabled = false)
+  @Test(enabled = true)
   public void testModifyGroup() {
-    List<GroupData> before = app.group().list();
-    int index = before.size()-1;
+    Set<GroupData> before = app.group().all();
+    GroupData modifiedGroup = before.iterator().next();
     GroupData group = new GroupData()
-            .withId(before.get(index).getId())
+            .withId(modifiedGroup.getId())
             .withName("group updated")
             .withHeader("header updated")
             .withFooter("footer updated");
-    app.group().modify(index, group);
-    List<GroupData> after = app.group().list();
+    app.group().modify(group);
+    Set<GroupData> after = app.group().all();
     Assert.assertEquals(after.size(),before.size());
 
-    before.remove(index);
+    before.remove(modifiedGroup);
     before.add(group);
-    Comparator<? super GroupData> byId = (g1,g2) -> Integer.compare(g1.getId(), g2.getId());
-    before.sort(byId);
-    after.sort(byId);
     Assert.assertEquals(before, after);
   }
 }
